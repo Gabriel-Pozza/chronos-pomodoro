b2bfac094487cd73a3df4a84423b559c2600c98b
Gabriel Pozza <gabriel.pozza10@hotmail.com>
2026-04-09 21:53:57 -0300

    Criando Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { Heading } from './components/Heading'
+import './styles/theme.css';
+import './styles/Global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
-import './styles/theme.css'
-import './styles/Global.css'
-import { TimerIcon } from 'lucide-react'
+export function App() {
+  return (
+    <>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
 
+      <Container>
+          <Heading>MENU</Heading>
+      </Container>
 
-export function App(){
+      <Container>
+          <Heading>FORM</Heading>
+      </Container>
 
-    return (
-
-        <div>
-            <Heading>Ola mundoaasasa
-                <button>
-                    <TimerIcon/>
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat mollitia doloremque, beatae consequuntur aliquam hic autem praesentium cupiditate accusantium molestias saepe enim, tempore veritatis obcaecati facere, iusto ipsum animi consequatur.</p>
-        </div>
-    )
-
-
-
+      <Container>
+          <Heading>FOOTER</Heading>
+      </Container>
+      
+    </>
+  );
 }
