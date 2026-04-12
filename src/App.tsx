ecad01c7007ba85887e81430540d32eb5c68e8b5
Gabriel Pozza <gabriel.pozza10@hotmail.com>
2026-04-11 21:27:29 -0300

    Adicionando tema claro e escuro com UseState e UseEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/Global.css';
+
+
+
+
+
 
 
 export function App() {
@@ -14,16 +25,38 @@ export function App() {
       </Container>
 
       <Container>
-          <Heading>MENU</Heading>
+          <Menu />
       </Container>
 
       <Container>
-          <Heading>FORM</Heading>
+          <CountDown />
       </Container>
 
       <Container>
-          <Heading>FOOTER</Heading>
+          <form className='form' action="">
+            <div className="formRow">
+              <DefaultInput labelText='task' id='meuInput' type='text' placeholder='Digite algo'
+              />
+            </div>
+
+            <div className="formRow">
+             <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className="formRow">
+              <Cycles />
+            </div>
+
+            <div className="formRow">
+             <DefaultButton icon={<PlayCircleIcon />} color='green'/>
+            </div>
+          </form>
       </Container>
+
+       <Container>
+          <Footer />
+      </Container>
+
       
     </>
   );
