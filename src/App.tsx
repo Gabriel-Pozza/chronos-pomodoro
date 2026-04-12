67481f643c3e79e591ccaee3e4871bb1bca17d67
Gabriel Pozza <gabriel.pozza10@hotmail.com>
2026-04-12 00:28:43 -0300

    Adicionado Pags Not Found/AboutPomodoro e Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,15 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/Global.css';
-
-
-
-
-
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 
 export function App() {
   return (
     <>
-      <Container>
-        <Logo/>
-      </Container>
-
-      <Container>
-          <Menu />
-      </Container>
-
-      <Container>
-          <CountDown />
-      </Container>
-
-      <Container>
-          <form className='form' action="">
-            <div className="formRow">
-              <DefaultInput labelText='task' id='meuInput' type='text' placeholder='Digite algo'
-              />
-            </div>
-
-            <div className="formRow">
-             <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-
-            <div className="formRow">
-              <Cycles />
-            </div>
-
-            <div className="formRow">
-             <DefaultButton icon={<PlayCircleIcon />} color='green'/>
-            </div>
-          </form>
-      </Container>
-
-       <Container>
-          <Footer />
-      </Container>
-
-      
+      <Home />
     </>
   );
 }
